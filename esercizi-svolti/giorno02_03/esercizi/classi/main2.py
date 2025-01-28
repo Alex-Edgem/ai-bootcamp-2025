80d58ae4fcf5e7946fa3e5fa57a45cdad3a36dc9
alex <greco.alessandro.indagini@gmail.com>
2025-01-28 08:42:25 +0100

    refactoring function con nozioni lezione 3 e classi con iterarator

--- esercizi-svolti/giorno02_03/esercizi/classi/main2.py
+++ esercizi-svolti/giorno02_03/esercizi/classi/main2.py
@@ -6,26 +6,23 @@ class Country:
     def add(self, region):
         self.regions.append(region)
 
-    # usiamo ciclo annidato
-    @property
-    def pop(self):
-        popolazione_totale = 0
-        for region in self.regions:
-            for city in region.cities:
-                popolazione_totale += city.pop
-        return popolazione_totale
+    def __iter__(self):
+        return iter(self.regions)
+
+
 
     # usiamo list comprehension
     @property
-    def pop2(self):
-        return sum(city.pop for region in self.regions for city in region.cities)
+    def pop(self):
+        return sum(city.pop for region in self for city in region)
+
 
     @property
     def most_populuous_city(self):
         most_populus = 0
         current_most_city = None
-        for region in self.regions:
-            for city in region.cities:  # verificare se si puo rendere iterabile
+        for region in self:
+            for city in region:
                 if city.pop > most_populus:
                     most_populus = city.pop
                     current_most_city = city
@@ -33,23 +30,26 @@ class Country:
 
 
 class Region:
-    def __init__(self, name):
+    def __init__(self,name):
         self.name = name
         self.cities = []
 
     def add(self, city):
         self.cities.append(city)
 
+    def __iter__(self):
+        return iter(self.cities)
+
     @property
     def pop(self):
-        return sum(city.pop for city in self.cities if city.pop is not None)
+        return sum(city.pop for city in self if city.pop is not None)
+
 
 
 class City:
     def __init__(self, name, pop=None):
         self.name = name
         self.pop = pop
-
 
 italy = Country("Italy")
 sicily = Region("Sicily")
@@ -61,6 +61,7 @@ catania = City("Catania", 300_000)
 reggio = City("Reggio", 170_000)
 gallipoli = City("Gallipoli", 1_000_000)
 
+
 italy.add(sicily)
 italy.add(calabria)
 italy.add(puglia)
@@ -69,3 +70,5 @@ sicily.add(palermo)
 sicily.add(catania)
 calabria.add(reggio)
 puglia.add(gallipoli)
+
+print(italy.pop)
