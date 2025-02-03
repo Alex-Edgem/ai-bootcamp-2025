7cb1ac7685a8f0cfde59278ad98982160c460749
alex <greco.alessandro.indagini@gmail.com>
2025-02-03 15:14:26 +0100

    commit modifiche esercizi giorno 5

--- esercizi-svolti/giorno04/main.py
+++ esercizi-svolti/giorno04/main.py
@@ -29,15 +29,3 @@ class Business:
         self.name = name
         self.phone = phone
 
-directory = Directory()
-
-assert len(directory) == 0
-
-directory.add(Person(name="Margaret", surname="Hamilton", phone="01-234-567"))
-directory.add(Business(name="Vedrai", phone="+39-333-333333"))
-directory.add(Person(name="Linda", surname="Hamilton"))
-
-assert len(directory) == 3
-
-assert [el.phone for el in directory.query(name="Vedrai")] == ["+39-333-333333"]
-assert [el.phone for el in directory.query(name="Margaret")] == ["01-234-567"]
